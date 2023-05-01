6838da63d7d0fb58fca7143d0aca6aea155b1fbd
preciousimo <preciousimoniakemu@gmail.com>
2023-05-01 11:23:01 +0100

    Create static messages page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import FeedView from '../views/FeedView.vue'
 import SignupView from '../views/SignupView.vue'
 import LoginView from '../views/LoginView.vue'
+import MessagesView from '../views/MessagesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: MessagesView
     },
     {
       path: '/about',
