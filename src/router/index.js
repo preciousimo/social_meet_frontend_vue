27d45fa69eff3f6b4198fbab660f8386a6dcf64f
preciousimo <preciousimoniakemu@gmail.com>
2023-05-01 10:11:47 +0100

    Create static log in page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignupView from '../views/SignupView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignupView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/about',
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
         <div class="main-left">
             <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Sign up</h1>
+                <h1 class="mb-6 text-2xl">Log in</h1>
 
                 <p class="mb-6 text-gray-500">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ex aperiam minus placeat at saepe
@@ -10,19 +10,14 @@
                 </p>
 
                 <p class="font-bold">
-                    Already have an account? <RouterLink :to="{'name': 'login'}" class="underline">Click here</RouterLink> to sign in.
+                    Don't have an account? <RouterLink :to="{'name': 'signup'}" class="underline">Click here</RouterLink> to create one.
                 </p>
             </div>
         </div>
 
         <div class="main-right">
             <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <form class="space-y-6">
-                    <div>
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-600">Name</label>
-                        <input type="text" placeholder="Your full name" id="name" name="name" class="w-full px-4 py-2 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:border-gray-600">
-                    </div>
-
+                <form class="space-y-6"> 
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-600">Email</label>
                         <input type="email" placeholder="Your e-mail address" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:border-gray-600">
@@ -31,15 +26,10 @@
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-600">Password</label>
                         <input type="password" placeholder="Your password" id="password" name="password" class="w-full px-4 py-2 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:border-gray-600">
-                    </div>
+                    </div> 
 
                     <div>
-                        <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-600">Confirm Password</label>
-                        <input type="password" placeholder="Repeat your password" id="password_confirmation" name="password_confirmation" class="w-full px-4 py-2 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:border-gray-600">
-                    </div>
-
-                    <div>
-                        <button type="submit" class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-purple-800 rounded-lg hover:bg-purple-700 focus:outline-none focus:bg-gray-700">Sign up</button>
+                        <button type="submit" class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-purple-800 rounded-lg hover:bg-purple-700 focus:outline-none focus:bg-gray-700">Log in</button>
                     </div>
                 </form>
             </div>
