83d4bffe3dbf4f0f887b19fc99e2726688f20437
preciousimo <preciousimoniakemu@gmail.com>
2023-05-05 22:02:57 +0100

    View user

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-        <div class="main-center col-span-3 space-y-4">
+        <div class="main-left cols-span-1">
+            <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+                <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full" />
+
+                <p><strong>{{ userStore.user.name}}</strong></p>
+
+                <div class="mt-6 flex space-x-8 justify-around">
+                    <p class="text-xs text-gray-500">103 friends</p>
+                    <p class="text-xs text-gray-500">10 posts</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="main-center col-span-2 space-y-4">
             <div class="bg-white border border-gray-200 rounded-lg">
                 <form v-on:submit.prevent="submitForm" method="post">
                     <div class="p-4">  
@@ -80,8 +93,17 @@
 import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
 import Trends from '../components/Trends.vue'
+import { useUserStore } from '@/stores/user'
 export default {
     name: 'FeedView',
+
+    setup() {
+        const userStore = useUserStore()
+        return {
+            userStore
+        }
+    },
+
     components: {
         PeopleYouMayKnow,
         Trends,
@@ -101,7 +123,7 @@ export default {
     methods: {
         getFeed() {
             axios
-                .get('/api/posts/')
+                .get(`/api/posts/profile/${this.$route.params.id/}`)
                 .then(response => {
                     console.log('data', response.data)
 
