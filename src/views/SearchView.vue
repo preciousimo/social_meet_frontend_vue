7fc911d779771c5ad6bd679ebd29069874fef95d
preciousimo <preciousimoniakemu@gmail.com>
2023-05-05 23:16:18 +0100

    Convert feed to seperate component

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,31 +2,32 @@
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
         <div class="main-left col-span-3 space-y-4">
             <div class="bg-white border border-gray-200 rounded-lg">
-                <form v-on:submit.prevent="submitForm" class="p-4 flex space-x-4">  
-                    <input v-model="query" type="search" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you looking for?">
+                <form v-on:submit.prevent="submitForm" class="p-4 flex space-x-4">
+                    <input v-model="query" type="search" class="p-4 w-full bg-gray-100 rounded-lg"
+                        placeholder="What are you looking for?">
 
                     <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                         </svg>
                     </button>
                 </form>
             </div>
 
-            <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4"
+            <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4" 
                 v-if="users.length"
             >
-                <div 
-                    class="p-4 text-center bg-gray-100 rounded-lg"
-                    v-for="user in users"
+                <div class="p-4 text-center bg-gray-100 rounded-lg" 
+                    v-for="user in users" 
                     v-bind:key="user.id"
                 >
                     <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
 
                     <p>
                         <strong>
-                            <RouterLink :to="{name: 'profile', params:{'id': user.id}}">{{ user.name }}</RouterLink>
+                            <RouterLink :to="{ name: 'profile', params: { 'id': user.id } }">{{ user.name }}</RouterLink>
                         </strong>
                     </p>
 
@@ -38,67 +39,17 @@
             </div>
 
             <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg"
-                v-for="post in posts"
+                class="p-4 bg-white border border-gray-200 rounded-lg" 
+                v-for="post in posts" 
                 v-bind:key="post.id"
             >
-                <div class="mb-6 flex items-center justify-between">
-                    <div class="flex items-center space-x-6">
-                        <img src="https://i.pravatar.cc/300?img=10" class="w-[40px] rounded-full">
-
-                        <p>
-                            <strong> 
-                                <RouterLink :to="{name: 'profile', params:{'id':post.created_by.id}}">{{ post.created_by.name }}</RouterLink>
-                            </strong>
-                        </p>
-                    </div>
-
-                    <p class="text-gray-600">{{ post.created_at_formatted }}</p>
-                </div>
-
-                <p>{{ post.body }}</p>
-
-                <div class="my-6 flex justify-between">
-                    <div class="flex space-x-6">
-                        <div class="flex items-center space-x-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
-                                </path>
-                            </svg>
-
-                            <span class="text-gray-500 text-xs">82 likes</span>
-                        </div>
-
-                        <div class="flex items-center space-x-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z">
-                                </path>
-                            </svg>
-
-                            <span class="text-gray-500 text-xs">0 comments</span>
-                        </div>
-                    </div>
-
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z">
-                            </path>
-                        </svg>
-                    </div>
-                </div>
+                <FeedItem v-bind:post="post" />
             </div>
-        </div>
+            <div class="main-right col-span-1 space-y-4">
+                <PeopleYouMayKnow />
 
-        <div class="main-right col-span-1 space-y-4">
-            <PeopleYouMayKnow />
-
-            <Trends />
+                <Trends />
+            </div>
         </div>
     </div>
 </template>
@@ -106,15 +57,15 @@
 <script>
 import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
-import Trends from '../components/Trends.vue' 
-import { RouterLink } from 'vue-router'
+import Trends from '../components/Trends.vue'
+import FeedItem from '../components/FeedItem.vue'
 export default {
     name: 'SearchView',
     components: {
-    PeopleYouMayKnow,
-    Trends,
-    RouterLink
-},
+        PeopleYouMayKnow,
+        Trends,
+        FeedItem,
+    },
     data() {
         return {
             query: '',
