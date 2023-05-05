ca9b1b06107427a7c004a4dae25decfdcf03dc77
preciousimo <preciousimoniakemu@gmail.com>
2023-05-05 22:54:32 +0100

    Setup basic search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,62 +2,33 @@
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
         <div class="main-left col-span-3 space-y-4">
             <div class="bg-white border border-gray-200 rounded-lg">
-                <div class="p-4 flex space-x-4">
-                    <input type="search" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you looking for?">
+                <form v-on:submit.prevent="submitForm" class="p-4 flex space-x-4">  
+                    <input v-model="query" type="search" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you looking for?">
 
-                    <a href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Post</a>
-                </div>
+                    <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                        </svg>
+                    </button>
+                </form>
             </div>
 
-            <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4">
-                <div class="p-4 text-center bg-gray-100 rounded-lg">
+            <div 
+                class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4"
+                v-if="users.length"
+            >
+                <div 
+                    class="p-4 text-center bg-gray-100 rounded-lg"
+                    v-for="user in users"
+                    v-bind:key="user.id"
+                >
                     <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
 
-                    <p><strong>Code With Stein</strong></p>
-
-                    <div class="mt-6 flex space-x-8 justify-around">
-                        <p class="text-xs text-gray-500">182 friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
-                    </div>
-                </div>
-
-                <div class="p-4 text-center bg-gray-100 rounded-lg">
-                    <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
-
-                    <p><strong>Code With Stein</strong></p>
-
-                    <div class="mt-6 flex space-x-8 justify-around">
-                        <p class="text-xs text-gray-500">182 friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
-                    </div>
-                </div>
-
-                <div class="p-4 text-center bg-gray-100 rounded-lg">
-                    <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
-
-                    <p><strong>Code With Stein</strong></p>
-
-                    <div class="mt-6 flex space-x-8 justify-around">
-                        <p class="text-xs text-gray-500">182 friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
-                    </div>
-                </div>
-
-                <div class="p-4 text-center bg-gray-100 rounded-lg">
-                    <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
-
-                    <p><strong>Code With Stein</strong></p>
-
-                    <div class="mt-6 flex space-x-8 justify-around">
-                        <p class="text-xs text-gray-500">182 friends</p>
-                        <p class="text-xs text-gray-500">120 posts</p>
-                    </div>
-                </div>
-
-                <div class="p-4 text-center bg-gray-100 rounded-lg">
-                    <img src="https://i.pravatar.cc/300?img=10" class="mb-6 rounded-full">
-
-                    <p><strong>Code With Stein</strong></p>
+                    <p>
+                        <strong>
+                            <RouterLink :to="{name: 'profile', params:{'id': user.id}}">{{ user.name }}</RouterLink>
+                        </strong>
+                    </p>
 
                     <div class="mt-6 flex space-x-8 justify-around">
                         <p class="text-xs text-gray-500">182 friends</p>
@@ -66,18 +37,26 @@
                 </div>
             </div>
 
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
+            <div 
+                class="p-4 bg-white border border-gray-200 rounded-lg"
+                v-for="post in posts"
+                v-bind:key="post.id"
+            >
                 <div class="mb-6 flex items-center justify-between">
                     <div class="flex items-center space-x-6">
                         <img src="https://i.pravatar.cc/300?img=10" class="w-[40px] rounded-full">
 
-                        <p><strong>Code With Stein</strong></p>
+                        <p>
+                            <strong> 
+                                <RouterLink :to="{name: 'profile', params:{'id':post.created_by.id}}">{{ post.created_by.name }}</RouterLink>
+                            </strong>
+                        </p>
                     </div>
 
-                    <p class="text-gray-600">18 minutes ago</p>
+                    <p class="text-gray-600">{{ post.created_at_formatted }}</p>
                 </div>
 
-                <p>Lorem ipsum bla bla lbalkjasldkfj aslkjdf lkasjdfkljaslkfjalksjf</p>
+                <p>{{ post.body }}</p>
 
                 <div class="my-6 flex justify-between">
                     <div class="flex space-x-6">
@@ -125,13 +104,40 @@
 </template>
 
 <script>
+import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
-import Trends from '../components/Trends.vue'
+import Trends from '../components/Trends.vue' 
+import { RouterLink } from 'vue-router'
 export default {
     name: 'SearchView',
     components: {
-        PeopleYouMayKnow,
-        Trends,
+    PeopleYouMayKnow,
+    Trends,
+    RouterLink
+},
+    data() {
+        return {
+            query: '',
+            users: [],
+            posts: []
+        }
+    },
+    methods: {
+        submitForm() {
+            console.log('submitForm', this.query)
+            axios
+                .post('/api/search/', {
+                    query: this.query
+                })
+                .then(response => {
+                    console.log('response:', response.data)
+                    this.users = response.data.users
+                    this.posts = response.data.posts
+                })
+                .catch(error => {
+                    console.log('error:', error)
+                })
+        }
     }
 }
 </script>
