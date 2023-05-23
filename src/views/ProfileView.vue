84eefd2d720d964da2292065305dc4328d3991c9
preciousimo <preciousimoniakemu@gmail.com>
2023-05-23 19:43:02 +0100

    Chatting

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -23,6 +23,14 @@
                     </button>
 
                     <button 
+                        class="inline-block mt-4 py-4 px-3 bg-purple-600 text-xs text-white rounded-lg" 
+                        @click="sendDirectMessage"
+                        v-if="userStore.user.id !== user.id"
+                    >
+                        Send direct message
+                    </button>
+
+                    <button 
                         class="inline-block py-4 px-3 bg-red-600 text-xs text-white rounded-lg" 
                         @click="logout"
                         v-if="userStore.user.id === user.id"
@@ -119,6 +127,19 @@ export default {
     },
 
     methods: {
+        sendDirectMessage() {
+            console.log('sendDirectMessage')
+            axios
+                .get(`/api/chat/${this.$route.params.id}/get-or-create/`)
+                .then(response => {
+                    console.log(response.data)
+                    this.$router.push('/chat')
+                })
+                .catch(error => {
+                    console.log('error', error)
+                })
+        },
+        
         sendFriendshipRequest() {
             axios
                 .post(`/api/friends/${this.$route.params.id}/request/`)
