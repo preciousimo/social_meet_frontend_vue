2b199f530c5a80c4c2777c8c3dd1d56769350261
preciousimo <preciousimoniakemu@gmail.com>
2023-05-24 00:07:03 +0100

    Notifications

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import ChatView from '../views/ChatView.vue'
 import TrendView from '../views/TrendView.vue'
 import EditProfileView from '../views/EditProfileView.vue'
 import EditPasswordView from '../views/EditPasswordView.vue'
+import NotificationsView from '../views/NotificationsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -74,6 +75,11 @@ const router = createRouter({
       path: '/profile/edit/password',
       name: 'editpassword',
       component: EditPasswordView
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: NotificationsView
     },
     {
       path: '/about',
