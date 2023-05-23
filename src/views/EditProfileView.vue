91c96ce13924880b203bb75b9865cf958e7ed719
preciousimo <preciousimoniakemu@gmail.com>
2023-05-23 23:40:45 +0100

    Fix bug

--- src/views/EditProfileView.vue
+++ src/views/EditProfileView.vue
@@ -95,7 +95,7 @@ export default {
                                 id: this.userStore.user.id,
                                 name: this.form.name,
                                 email: this.form.email,
-                                avatar: 
+                                avatar: response.data.user.get_avatar
                             })
                             this.$router.back()
                         } else {
